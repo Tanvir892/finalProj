15829cf97ce6d564e4e02966d292ef1e82509589
Tanvir892 <mdtanvirahmed619@gmail.com>
2023-04-27 02:44:01 +0100

    Deployed website

--- movieRecSys/client/next.config.js
+++ movieRecSys/client/next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: process.env.NODE_ENV === "production" ? "/finalProj" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/finalProj" : "",
 };
 
 module.exports = nextConfig;
